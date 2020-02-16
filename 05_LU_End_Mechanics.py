2d1e151b64f166f863d1d72810b1f77b4ebb478a
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2020-02-17 09:33:01 +1300

    d

--- 05_LU_End_Mechanics.py
+++ 05_LU_End_Mechanics.py
@@ -1,29 +1,39 @@
 # Lucky unicorn - payment mechanics
 
 # To do
-#   - if it's a unicorn, add $15
-#   - if it's a zebra / horse, subtract 0.5
-#   - if it's a donkey, subtract 1
-# Give user feedback based on winnings
-# State new totals
+# Ask user how much money they have to play with
+# If the total is less than $1, quit
+# If the total is more than $1, ask user if they want to keep going
+
+
 # Assume starting amount is $10
-total = 10
+total = int(input("How much would you like to play with? "))
 
-# Allow manual token input for testing purposes
-token = input("Enter a token: ")
+keep_going = ""
+while keep_going == "":
 
-# Adjust total correctly for a given token
-if token == "unicorn":
-    total += 5
-    feedback = "Congratulations, you won $5.00"
-elif token == "donkey":
-    total -= 1
-    feedback = "Sorry you did not win anything in this round"
-else:
-    total += 0.5
-    feedback = "Congratulations, you won 50c"
-print()
+    # Allow manual token input for testing purposes
+    token = input("Enter a token: ")
 
-print(feedback)
-print("You have ${:.2f} to play with.".format(total))
+    # Adjust total correctly for a given token
+    if token == "unicorn" :
+        total += 5
+        feedback = "Congratulations, you won $5.00"
+    elif token == "donkey":
+        total -= 1
+        feedback = "Sorry you did not win anything in this round"
+    else:
+        total += 0.5
+        feedback = "Congratulations, you won 50c"
+    print()
 
+    print(feedback)
+    print("You have ${:.2f} to play with.".format(total))
+
+    if total < 1:
+        print("Sorry you don't have enough money to continue. Game over")
+        keep_going = "end"
+    else:
+        keep_going = input("Press <enter> to play again or any key to quit. ")
+
+print("Thank you for playing.")
