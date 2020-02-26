53f503f9ea8b2a1bcda88b9162697083f28d5e04
stevensc9277 <60583258+stevensc9277@users.noreply.github.com>
2020-02-26 13:11:44 +1300

    a

--- 01_variables_v2.py
+++ 01_variables_v2.py
@@ -18,11 +18,6 @@ def num_check(question, low, high):
 
 # Main routine
 num_1 = num_check("Enter a number between 1 and 10: ", 1, 10)
-num_2 = num_check("Enter a second number between one and ten: ", 1, 10)
 
-num_3 = num_1 + num_2
-# multiply two numbers together
-num_4 = num_1 * num_2
-# greet user and display math
-print("The numbers that you have chosen add up to {}".format(num_3))
-print("Your numbers also multiply to get {}".format(num_4))
+
+
